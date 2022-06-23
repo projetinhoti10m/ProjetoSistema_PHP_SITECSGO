01eb54ff8c741776020067a007c5cda31fdd68ac
projetinhoti10m <105933940+projetinhoti10m@users.noreply.github.com>
2022-06-23 16:51:43 -0300

    Update conexao.php

--- model/conexao.php
+++ model/conexao.php
@@ -1,8 +1,8 @@
 <?php
 
-$url = "localhost";
-$usuario ="root";
-$senha = "";
+$url = "major.mysql.database.azure.com";
+$usuario ="admti10m";
+$senha = "999237244_danilo";
 $nomeBanco ="major";
 
 $conn = mysqli_connect($url,$usuario,$senha,$nomeBanco);
